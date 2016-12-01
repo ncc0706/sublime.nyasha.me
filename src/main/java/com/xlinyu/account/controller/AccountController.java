229cd5024b44be642772c72be6693194208f78a8
ncc0706 <ncc0706@gmail.com>
2016-12-01 16:11:54 +0800

    用户相关

---
+++ src/main/java/com/xlinyu/account/controller/AccountController.java
@@ -0,0 +1,48 @@
+package com.xlinyu.account.controller;
+
+import com.xlinyu.account.model.User;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by gavin on 20/09/2016.
+ */
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+
+
+    private static final Logger logger = Logger.getLogger(AccountController.class);
+
+    @RequestMapping(value = "/signin", method = RequestMethod.GET)
+    public String signin(){
+        return "account/signin";
+
+    }
+
+    @RequestMapping(value = "/signin", method = RequestMethod.POST)
+    public String login(User user){
+        logger.info(user);
+        return "redirect:/";
+
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signup(){
+
+        return "account/signup";
+
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logout(HttpSession session){
+        session.invalidate();
+        return "redirect:/account/signin";
+    }
+
+}
